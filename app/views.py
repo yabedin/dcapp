e521d88f0b974b85e441d0784b897ba517915446
yabedin <yasmin_abedin@hotmail.co.uk>
2020-09-16 11:32:35 +0100

    preprocessing

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ import json
 import uuid
 
 # Import my files 
-from preprocessing import convert_to_csv
+from preprocessing import convert_to_csv, get_dr_speech
 
 @app.route("/")
 def index():
@@ -25,20 +25,28 @@ def getjson():
         # Save incoming JSON to a CSV file. Will only save if it's in the correct format i.e. IBM Watson's format
         
         # Generate unique id for CSV filename
-        unique_id = str(uuid.uuid4())[:8]
-        filename = unique_id
+        unique_id = str(uuid.uuid4())[:5] 
+        filename_full = unique_id + "_all_speech"
+        filename_dr = unique_id + "_dr_speech"
 
         # Check if data is in correct format 
-        if convert_to_csv.convert_to_csv(data, filename) == None:
+        if convert_to_csv.convert_to_csv(data, filename_full) == None:
             res = make_response(jsonify({"Message:":"Please send a JSON file from IBM Watson's STT API."}), 400)
             return res
 
         # Executes if JSON data is as expected 
         else:
             # Convert to CSV 
-            convert_to_csv.convert_to_csv(data, filename)
+            convert_to_csv.convert_to_csv(data, filename_full)
+
+            # Get dr speech 
+            dr_speech_list = get_dr_speech.get_dr_speech(data, 1)
+            get_dr_speech.write_csv(filename_dr, dr_speech_list)
 
 
+
+
+            # Put through LR model 
 
             # Need to return report eventually.... 
             response = {
